95cbbb92f613c12b193ffb0293c7abb20a9a1293
soer <474436731@qq.com>
2023-10-24 09:39:28 +0800

    demo

--- Pods/Target Support Files/Pods-KRWebView/Pods-KRWebView-frameworks.sh
+++ Pods/Target Support Files/Pods-KRWebView/Pods-KRWebView-frameworks.sh
@@ -187,6 +187,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/flutter_boost/flutter_boost.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/mmkvflutter/mmkvflutter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/path_provider_foundation/path_provider_foundation.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/shared_preferences_foundation/shared_preferences_foundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/url_launcher_ios/url_launcher_ios.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
@@ -201,6 +202,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/flutter_boost/flutter_boost.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/mmkvflutter/mmkvflutter.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/path_provider_foundation/path_provider_foundation.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/shared_preferences_foundation/shared_preferences_foundation.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/url_launcher_ios/url_launcher_ios.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
